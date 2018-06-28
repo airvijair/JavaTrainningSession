6592ba1132ceed4220d76b5fcb243009594e2608
VijayaKumarRamasamy <vijaykumar.ramasamy@wenable.com>
2018-06-28 18:58:55 +0530

    newly added

--- src/AbstractionConcept/BMW.java
+++ src/AbstractionConcept/BMW.java
@@ -1,5 +1,22 @@
 package AbstractionConcept;
 
-public class BMW {
+public class BMW implements Car{
 
+	@Override
+	public void start() {
+		System.out.println("BMW --- Start");
+		
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("BMW --- Stop");		
+	}
+
+	@Override
+	public void refule() {
+		System.out.println("BMW --- refule");		
+	}
+	
+	 
 }
--- src/AbstractionConcept/Car.java
+++ src/AbstractionConcept/Car.java
@@ -1,5 +1,9 @@
 package AbstractionConcept;
 
 public interface Car {
+	public void start();
+	public void stop();
+	public void refule();
+	
 
 }
